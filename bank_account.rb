e11915f5a77773de27b2112ec764abf4c2a2d817
Situ William <situ_william@yahoo.com>
2017-11-06 14:15:12 -0500

    finished bank account

--- bank_account.rb
+++ bank_account.rb
@@ -1,5 +1,5 @@
 class BankAccount
-	@@interest_rate = 0.0
+	@@interest_rate = 0.01
 	@@accounts = []
 	def initialize
 		@balance = 0
@@ -29,15 +29,18 @@ class BankAccount
 
 	def self.total_funds
 		sum = 0.0
-		@@accounts.each do |acc_bal|
-			sum += acc_bal
+
+		@@accounts.each do |current_acc|
+			sum += current_acc.balance
 		end
 		return sum
 	end
 
 	def self.interest_time
-		@@accounts.map! do |interest_bal|
-			interest_bal = @balance + @balance * @interest_rate
+		new_bal = 0
+		@@accounts.each do |interest_bal|
+			new_bal = interest_bal.balance + interest_bal.balance * @@interest_rate
+			interest_bal.set_balance(new_bal)
 		end
 	end
 end
@@ -50,11 +53,12 @@ my_account.deposit(200)
 your_account.deposit(1000)
  puts my_account.balance # 200
  puts your_account.balance # 1000
+
  puts BankAccount.total_funds # 1200
-# BankAccount.interest_time
-# puts my_account.balance # 202.0
-# puts your_account.balance # 1010.0
-# puts BankAccount.total_funds # 1212.0
-# my_account.withdraw(50)
-# puts my_account.balance # 152.0
-# puts BankAccount.total_funds # 1162.0
+BankAccount.interest_time
+puts my_account.balance # 202.0
+puts your_account.balance # 1010.0
+puts BankAccount.total_funds # 1212.0
+my_account.withdraw(50)
+puts my_account.balance # 152.0
+puts BankAccount.total_funds # 1162.0
